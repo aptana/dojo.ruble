38aa7fa825cbd207c0d3cc28c84b63d7d91c9633
Ingo Muschenetz <ingo@appcelerator.com>
2011-04-15 21:44:58 -0700

    first commit

---
+++ bundle.rb
@@ -0,0 +1,8 @@
+require 'ruble'
+
+bundle 'Dojo' do |bundle|
+  bundle.author = 'Ingo Muschenetz'
+  bundle.description =  <<END
+Support for the <a href="http://dojo.org/">Dojo JavaScript library</a>.
+END
+end
